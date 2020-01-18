e88fc3fb9ed5ca4e0e15b84b49479b0b72af1896
jensjeflensje <jensgamesnl@gmail.com>
2020-01-18 18:46:36 +0100

    Initial commit

---
+++ bot.js
@@ -0,0 +1,31 @@
+const { Client, RichEmbed } = require("discord.js");
+const { get } = require("superagent");
+const { prefix, token } = require("./config.json");
+const bot = new Client();
+
+bot.on('ready', () => {
+    console.log(`${bot.user.tag} werkt nu als simple_moppenbot`);
+});
+
+bot.on('message', message => {
+    if (message.content === `${prefix}mop`) {
+        get("https://moppenbot.nl/api/random")
+            .then(res => {
+                if (!res.body.success) {
+                    message.channel.send("MoppenBot API gaf een error aan.");
+                    return;
+                }
+                let jokeObj = res.body.joke;
+                let embed = new RichEmbed()
+                    .setTitle(`Mop van ${jokeObj.author}`)
+                    .setDescription(jokeObj.joke)
+                    .setFooter(`${jokeObj.likes} likes`);
+                message.channel.send(embed);
+            })
+            .catch(err => {
+                message.channel.send("HTTP request gaf een error aan.")
+            });
+    }
+});
+
+bot.login(token);
